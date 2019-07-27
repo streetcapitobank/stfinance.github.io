38b62ee0a618b1494aaac3548b270f5c372ef30b
stfinance <52859055+stfinance@users.noreply.github.com>
2019-07-27 11:32:08 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,53 @@
+function check(form)
+		{
+		
+		
+		
+		if(form.accnm.value == "rukundo" && form.pswd.value == "123")
+		{open('Rukundo.html')}
+		
+		else
+		{
+		
+		
+		
+		if(form.accnm.value == "wanduyi" && form.pswd.value == "123")
+		{window.open('Wanduyi.html')}
+		
+		else
+		{
+		
+		
+		
+		if(form.accnm.value == "kamoga" && form.pswd.value == "123")
+		{window.open('Kamoga.html')}
+		
+		else
+		{
+		
+		
+		if(form.accnm.value == "mulema" && form.pswd.value == "123")
+		{window.open('Mulema.html')}
+		
+		else
+		{alert("Account Number or Pin Number Error. Try Again")}
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		}
+		
+		
+		
+		
+		}
+		
